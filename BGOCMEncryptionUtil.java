47eacc24a25ede60ca12c565b4e72b4cccafe030
nagmorla <nagmorla@users.noreply.github.com>
2019-09-04 15:40:18 +0530

    Create BGOCMEncryptionUtil.java

---
+++ BGOCMEncryptionUtil.java
@@ -0,0 +1,82 @@
+package oracle.apps.bgocm.service.oid;
+
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class BGOCMEncryptionUtil {
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+
+    public BGOCMEncryptionUtil() {
+        super();
+    }
+    
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    private SecretKey getKey() throws Exception {
+        String strSaltKey = "OimSupportKeyForBGOCMApp";
+        byte[] arrayBytes = strSaltKey.getBytes(UNICODE_FORMAT);
+        KeySpec objKeySpec = new DESedeKeySpec(arrayBytes);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(DESEDE_ENCRYPTION_SCHEME);
+        SecretKey key = skf.generateSecret(objKeySpec);
+        return key;
+    }
+    
+    /**
+     *
+     * @param strClearText
+     * @return
+     */
+    public String encrypt(String strClearText) {
+        String encryptedString = null;
+        try {
+            Cipher cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME);
+            cipher.init(Cipher.ENCRYPT_MODE, getKey());
+            byte[] plainText = strClearText.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+    
+    /**
+     *
+     * @param strEncryptedText
+     * @return
+     */
+    public String decrypt(String strEncryptedText) {
+        String decryptedText = null;
+        try {
+            Cipher cipher = Cipher.getInstance(DESEDE_ENCRYPTION_SCHEME);
+            cipher.init(Cipher.DECRYPT_MODE, getKey());
+            byte[] encryptedText = Base64.decodeBase64(strEncryptedText);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText = new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+    
+    public static void main(String arg[]){
+        try{
+            BGOCMEncryptionUtil ed = new BGOCMEncryptionUtil();
+            System.out.println(""+ed.encrypt("stgs2madm2n"));
+            //System.out.println(""+ed.decrypt("8tRbLZ9bl1c7VzTFTl1bQQ=="));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
